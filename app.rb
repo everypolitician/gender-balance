821fccb4a71ab565f11e279d0ab515903f65b5fb
Chris Mytton <chrism@mysociety.org>
2015-06-26 10:54:21 +0100

    Add Google login

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
 
 get '/*.css' do |filename|
