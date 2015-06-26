8a2970b5034e77d86b5656b6672e0865281b6688
Zarino Zappia <mail@zarino.co.uk>
2015-06-26 16:34:48 +0100

    styling for messages/alerts, and better colours

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+var showMessages = function showMessages(hideAfterMilliseconds){
+  $('.app-messages').addClass('slide-in');
+  if(typeof hideAfterMilliseconds !== 'undefined') {
+    setTimeout(hideMessages, hideAfterMilliseconds);
+  }
+}
+
+var hideMessages = function hideMessages(){
+  $('.app-messages').removeClass('slide-in');
+  setTimeout(function(){
+    $('.app-messages').empty();
+  }, 1000);
+}
+
+$(function(){
+
+  // Animate any messages that have been sent from the server
+  if($('.app-messages *').length){
+    setTimeout(function(){
+      showMessages(3000);
+    }, 500);
+  }
+
+});
