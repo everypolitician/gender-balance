430e4460723c1636c3b5ae79bbd6b80ba3d9fde6
Chris Mytton <chrismytton@gmail.com>
2015-06-26 08:22:03 +0100

    Add Facebook login

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ use Rack::Flash
 
 use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 end
 
 get '/*.css' do |filename|
