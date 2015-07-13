6c6645022619beacd9e956e0d92d1eeb9fec220c
Chris Mytton <chrism@mysociety.org>
2015-07-13 16:02:49 +0100

    Join cache key with a colon
    This makes it easier to see which parts of the cache key have come from
    where when it scrolls past in the terminal.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,7 +5,7 @@ module Helpers
   end
 
   def csv_for(ref, path, last_modified)
-    cache [ref, path, last_modified].join(''), expiry: 1.day do
+    cache [ref, path, last_modified].join(':'), expiry: 1.day do
       csv_url = 'https://cdn.rawgit.com/everypolitician/everypolitician-data/' \
         "#{ref}/#{path}"
       CSV.parse(open(csv_url).read, headers: true, header_converters: :symbol)
