4e2a6a2f8fce55873c44d14296934fda026912c4
Chris Mytton <chrism@mysociety.org>
2015-07-15 10:31:59 +0100

    Add method for retrieving people with enough votes
    When someone has enough votes they can be submitted back to
    everypoltician for inclusion in the data.

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,4 +6,16 @@ class Response < Sequel::Model
     super
     validates_includes %w(male female other skip), :choice
   end
+
+  def self.with_votes(vote_count)
+    select(:politician_id, :country_code, :legislature_slug)
+      .exclude(choice: 'skip')
+      .group(:politician_id, :country_code, :legislature_slug)
+      .having('count(choice) >= ?', vote_count)
+  end
+
+  # If people have answered both male and female then there are conflicts
+  def no_conflicts?
+    Response.where(politician_id: politician_id).group_and_count(:choice).count == 1
+  end
 end
