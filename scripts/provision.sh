11692e9b6c99e0971443416501bf0a1ba8a17f90
Chris Mytton <chrismytton@gmail.com>
2015-07-28 16:44:59 +0100

    Add redis-server package to provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 # Install required packages
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   ruby2.2 ruby2.2-dev git build-essential libxslt1-dev libssl-dev \
-  postgresql libpq-dev memcached
+  postgresql libpq-dev memcached redis-server
 
 sudo -u postgres createuser --createdb vagrant
 createdb gender_crowdsourcing_development
