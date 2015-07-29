02c5710e93ed0fb5a6d30b6befbe3a2bf3e4a77f
Chris Mytton <chrism@mysociety.org>
2015-07-29 11:31:12 +0100

    Show level complete message after onboarding

--- public/js/main.js
+++ public/js/main.js
@@ -54,6 +54,7 @@ var filterElements = function filterElements(){
 function saveResponse(response) {
   if(window.onboarding) {
     if ($('.js-extra-cards li, .js-cardswipe li').length === 0){
+      $('.level-complete').fadeIn(100);
       return $.ajax({
         url: '/onboarding-complete',
         method: 'POST'
