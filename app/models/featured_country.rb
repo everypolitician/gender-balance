f1d438f712aad2894ae5dab3fbb9a43a30dcddab
Chris Mytton <chrism@mysociety.org>
2015-09-29 14:09:07 +0100

    Add a FeaturedCountry.current= method
    This makes it easy to set the current featured country using the country
    code. If the country code is invalid an error will be raised. If the
    country has already been featured then an error will also be raised as
    there are currently issues with featuring a country twice.

--- app/models/featured_country.rb
+++ app/models/featured_country.rb
@@ -5,5 +5,21 @@ class FeaturedCountry < Sequel::Model
         .where{end_date >= DateTime.now}
         .first
     end
+
+    def current=(country_code)
+      db.transaction do
+        current.update(end_date: DateTime.now) if current
+        country = Country.find_by_code(country_code)
+        raise ArgumentError, "Invalid country code: #{country_code}" if country.nil?
+        if FeaturedCountry.where(country_code: country_code).any?
+          raise ArgumentError, "This country has already been featured. See http://git.io/vcO2x"
+        end
+        FeaturedCountry.create(
+          country_code: country[:code],
+          start_date: DateTime.now,
+          end_date: 1.month.from_now
+        )
+      end
+    end
   end
 end
