616b58dbc45cdfec96549a7dbda392ebe77b6f1b
Chris Mytton <chrism@mysociety.org>
2016-02-16 14:36:23 +0000

    Tests for User#recent_countries
    Ensure that only incomplete recent countries are returned.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -21,4 +21,38 @@ describe User do
       assert_equal 'alice@example.org', user.name
     end
   end
+
+  describe '#recent_countries' do
+    let(:auth) { { provider: 'twitter', uid: '123', info: { name: 'Alice' } } }
+    let(:user) { User.create_with_omniauth(auth) }
+
+    before do
+      1.upto(3) do |n|
+        CountryUUID.create(
+          country_slug: 'Australia',
+          legislature_slug: 'Senate',
+          uuid: "au-#{n}"
+        )
+        CountryUUID.create(
+          country_slug: 'Germany',
+          legislature_slug: 'Bundestag',
+          uuid: "de-#{n}"
+        )
+      end
+    end
+
+    it 'includes countries a user has voted for' do
+      user.add_vote(person_uuid: 'au-1', choice: 'female')
+      user.add_vote(person_uuid: 'de-1', choice: 'male')
+      assert_equal ['Germany', 'Australia'], user.recent_countries.map(&:name)
+    end
+
+    it "doesn't include completed countries" do
+      user.add_vote(person_uuid: 'de-1', choice: 'male')
+      user.add_vote(person_uuid: 'au-1', choice: 'female')
+      user.add_vote(person_uuid: 'au-2', choice: 'female')
+      user.add_vote(person_uuid: 'au-3', choice: 'female')
+      assert_equal ['Germany'], user.recent_countries.map(&:name)
+    end
+  end
 end
