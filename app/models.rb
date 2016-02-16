e38b00289641d3d9d7b8ef37a7c409297ef209c7
Chris Mytton <chrism@mysociety.org>
2016-02-16 15:41:50 +0000

    Remove legacy unused models
    Now that we've fully moved over to using the votes table instead of
    responses these models are no longer needed.

--- app/models.rb
+++ app/models.rb
@@ -5,9 +5,6 @@ Sequel::Model.plugin :timestamps
 Sequel::Model.plugin :validation_helpers
 
 require 'app/models/user'
-require 'app/models/response'
-require 'app/models/country_count'
-require 'app/models/legislative_period'
 require 'app/models/featured_country'
 require 'app/models/vote'
 require 'app/models/country_uuid'
--- app/models/country_count.rb
+++
@@ -1,6 +0,0 @@
-# Caches a count of people in a country
-class CountryCount < Sequel::Model
-  def already_has_gender_data?
-    person_count == gender_count
-  end
-end
